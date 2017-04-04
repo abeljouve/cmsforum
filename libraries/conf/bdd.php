2d42d9d4825d39432e4aeea330382a88df88e030
Abel <abel.jouve@live.fr>
2017-04-04 16:37:53 +0200

    Update bdd.php

--- libraries/conf/bdd.php
+++ libraries/conf/bdd.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-  'hostname' => 'sql.majestrique.fr',
+  'hostname' => '127.0.0.1',
   'port' => 3306,
-  'username' => 'w3rx6l_forum',
-  'password' => 'azerty63',
-  'dbname' => 'w3rx6l_forum',
+  'username' => '',
+  'password' => '',
+  'dbname' => '',
 );
 ?>
